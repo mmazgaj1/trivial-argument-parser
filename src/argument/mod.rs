8ff50fb8b519ee95809ab1f83d6e394c39548ba6
Marcin Mazgaj <mmazgaj@protonmail.com>
2023-06-19 22:44:20 +0200

    Docs.
    Added description for legacy argument module.

--- src/argument/mod.rs
+++ src/argument/mod.rs
@@ -1,4 +1,6 @@
 pub mod builder;
+/// Legacy API argument module. Should not be use since parsable_argument is now the preffered way of defining arguments.
+/// Most likely will be removed in future.
 pub mod legacy_argument;
 pub mod parsable_argument;
 
